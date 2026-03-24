78442cef66de805a5f7fa6824e981138a051c5e2
theun1c <belovdima1122@yandex.ru>
2026-03-24 05:19:35 +0300

    test: add bounds checker test

--- internal/dungeon/grid_test.go
+++ internal/dungeon/grid_test.go
@@ -1,6 +1,8 @@
 package dungeon
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewGrid_InitializeWalls(t *testing.T) {
 
@@ -17,4 +19,15 @@ func TestNewGrid_InitializeWalls(t *testing.T) {
 			t.Fatalf("error: not wall after init")
 		}
 	}
+}
+
+func TestInBounds_IsInBounds(t *testing.T) {
+	w, h := 100, 100
+
+	g := NewGrid(w, h)
+
+	if !(g.InBounds(0, 1)) {
+		t.Fatalf("error: bounds error")
+	}
+
 }
