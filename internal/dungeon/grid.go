a5cba9de46660284b77afccc8beb2933e07f364e
theun1c <belovdima1122@yandex.ru>
2026-03-24 04:39:27 +0300

    feat: add grid init

--- internal/dungeon/grid.go
+++ internal/dungeon/grid.go
@@ -7,9 +7,32 @@ type Grid struct {
 }
 
 func NewGrid(width, height int) Grid {
+
+	cells := make([]Cell, width*height)
+
+	x, y := 0, 0
+
+	for i := range cells {
+
+		cells[i] = Cell{
+			X:    x,
+			Y:    y,
+			Tile: TileWall,
+		}
+
+		x++
+
+		if width == x {
+			y++
+			x = 0
+		}
+
+	}
+
 	return Grid{
 		Width:  width,
 		Height: height,
-		Cells:  make([]Cell, width*height),
+		Cells:  cells,
 	}
 }
+
